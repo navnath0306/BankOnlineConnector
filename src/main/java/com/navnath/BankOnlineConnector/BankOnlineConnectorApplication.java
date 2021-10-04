af50e9bd4d7143b15c6e63dee1533659f5eb7128
NAVNATH KADAM <kadamnavnath20@gmail.com>
2021-10-04 09:57:31 +0530

    Create spring boot project

---
+++ src/main/java/com/navnath/BankOnlineConnector/BankOnlineConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.navnath.BankOnlineConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOnlineConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOnlineConnectorApplication.class, args);
+	}
+
+}
