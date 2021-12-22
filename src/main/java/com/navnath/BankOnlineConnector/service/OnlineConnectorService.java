dcec2433ee475e23cb72f134069a6213fb9a5697
NAVNATH KADAM <kadamnavnath20@gmail.com>
2021-12-22 08:18:52 +0530

    Dummy bank test connectivity

--- src/main/java/com/navnath/BankOnlineConnector/service/OnlineConnectorService.java
+++ src/main/java/com/navnath/BankOnlineConnector/service/OnlineConnectorService.java
@@ -2,7 +2,12 @@ package com.navnath.BankOnlineConnector.service;
 
 import org.springframework.stereotype.Service;
 
+import com.navnath.BankOnlineConnector.Domain.AuthorizeRequest;
+import com.navnath.BankOnlineConnector.Domain.AuthorizeResponse;
+
 @Service
 public interface OnlineConnectorService {
+	public String testConnection();
+	public AuthorizeResponse authorize(AuthorizeRequest request);
 	
 }
